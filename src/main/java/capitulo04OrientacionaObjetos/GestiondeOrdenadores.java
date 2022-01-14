dfb31ebd6df68ef2b97e7f96ce4d985a11506e04
felipegonzalezjimenez12 <felipegonzalezjimenez12@gmail.com>
2022-01-14 11:30:38 +0100

    ggg

---
+++ src/main/java/capitulo04OrientacionaObjetos/GestiondeOrdenadores.java
@@ -0,0 +1,12 @@
+package capitulo04OrientacionaObjetos;
+
+public class GestiondeOrdenadores {
+
+	public static void main(String[] args) {
+		Ordenador java = new Ordenador();
+		java.altura = 40;
+		java.anchura= 50;
+
+	}
+
+}
---
+++ src/main/java/capitulo04OrientacionaObjetos/Ordenador.java
@@ -0,0 +1,13 @@
+package capitulo04OrientacionaObjetos;
+
+public class Ordenador {
+ boolean esPortatil;
+ int altura;
+ int anchura;
+ 
+ public void Imprime() {
+	 
+	 System.out.println("Propiedades son altura: " + altura + "anchura: " + anchura);
+	 
+ }
+}
