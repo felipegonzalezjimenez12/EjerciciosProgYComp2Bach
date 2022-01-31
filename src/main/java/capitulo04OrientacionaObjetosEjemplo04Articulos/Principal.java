f21ba82143563c94859d0a553bb48f82493ddc27
HIJIOS <HIJIOS@user-PC.home>
2022-01-31 23:24:26 +0100

    l

--- src/main/java/capitulo04OrientacionaObjetosEjemplo04Articulos/Principal.java
+++ src/main/java/capitulo04OrientacionaObjetosEjemplo04Articulos/Principal.java
@@ -5,78 +5,53 @@ import java.util.Scanner;
 
 public class Principal {
 
-	public static void main(String[] args) {
+	public static int main(String[] args) {
 		
+		
+		
+		int menu;
 		int opcionElegida;
-		do {
-			opcionElegida = menu;
-			switch  (opcionElegida) {
-			case 1: 
-				Acomestible acomestible = new Acomestible;
-				
-				System.out.println("codigo");
-				acomestible.serCodigo(sc.next());
-				System.out.println("precio");
-				System.out.println("caducidad");
-				
-
-				
-				
-				
-		
-			case 2:
-				Alimpieza alimpieza = new Alimpieza;
-			}
-			
-			
-			
-			
-			
-		}
-		
-		
-		public static int menu () {
-		
-		Scanner sc = new Scanner(System.in);
-		int opcion;
 		
 		System.out.println("Menú");
-		
 		System.out.println("0.- Salir");
 		System.out.println("1.- Crear articulo comestible");
 		System.out.println("2.- Crear articulo de limpieza");
 	
-		opcion = sc.nextInt();
+		Scanner sc = new Scanner (System.in);
+		menu = sc.nextInt();
+	
 		
-		}
 		
-		
-		while (opcion != 0) {
+		do {
+			
+			
+			
+			opcionElegida = menu;
+			switch  (opcionElegida) {
+			case 1: 
+				Acomestibles acomestible = new Acomestibles();
+				
+				System.out.println("codigo");
+				acomestible.setCodigo(sc.nextFloat());
+				System.out.println("precio");
+				acomestible.setPrecio(sc.nextFloat());
+				System.out.println("caducidad");
+				acomestible.setCaducidad(sc.toString());
+			
+			case 2:
+				Alimpieza alimpieza = new Alimpieza();
+				
+				System.out.println("codigo");
+				alimpieza.setCodigo(sc.nextFloat());
+				System.out.println("precio");
+				alimpieza.setPrecio(sc.nextFloat());
+				System.out.println("ignifugo");
+				alimpieza.setIgnifugo(sc.nextBoolean());
+			
+			}
 			
 		}
-		
-	
-	
-	
-	
-	
-	
-	
-	
+		while(opcionElegida != 0) ;
+		return opcionElegida;
+		}
 	}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-	
-	}
-
-
